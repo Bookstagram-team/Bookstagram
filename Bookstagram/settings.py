048be4c4408291098096f58a46b31ce6c1fcae90
SangMilyarder <nawawidarryl@gmail.com>
2023-10-27 12:07:47 +0700

    Dataset buku

--- Bookstagram/settings.py
+++ Bookstagram/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main',
+    'book',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'Bookstagram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ book/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from book.views import get_books
+app_name = 'booklist'
+
+urlpatterns = [
+    path("api/book/", get_books, name="get_books")
+]
