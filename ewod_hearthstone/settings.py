8890f9eb1c3e21fed04d7b5e6c5490b7f7e86612
ChillTheCode <edwin.daniel@ui.ac.id>
2023-11-30 22:45:58 +0700

    OTW DEPLOY

--- ewod_hearthstone/settings.py
+++ ewod_hearthstone/settings.py
@@ -138,11 +138,15 @@ USE_TZ = True
 #     os.path.join(BASE_DIR, 'static')
 # ]
 
+
+STATIC_URL = '/static/'
+
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 
-STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 AUTH_USER_MODEL = 'main.User'
